492a44300580ee200d8a1d1d38c51ec55dc0668a
Nicolas Marini <nmarini44@gmail.com>
2018-09-20 22:36:23 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,12 @@ class CreateCostumeStores < ActiveRecord::Migrations[5.1]
       i.string :name 
       i.string :location 
       i.integer :costume_inventory 
-      i.integer :employees_count 
+      i.integer :num_of_employees
+      i.boolean :still_in_business
+      i.integer :start_tme 
+      i.integer :closing_time 
+      
+      i.timestamps
     end 
   end 
 end 
